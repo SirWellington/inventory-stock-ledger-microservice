a3634c4869aebc9f2a03ad196eec0c13bbc17103
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-08-16 17:21:41 -0700

    Create EventPublisher.java

---
+++ src/main/java/com/sirwellington/target/producer/EventPublisher.java
@@ -0,0 +1,73 @@
+package com.sirwellington.target.producer;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sirwellington.target.model.EventPayload;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventPublisher {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EventPublisher.class);
+    private static final String TOPIC = KafkaProducerConfig.TOPIC;
+
+    private final KafkaProducer<String, String> producer;
+    private final ObjectMapper objectMapper;
+
+    public EventPublisher(
+        KafkaProducer<String, String> producer,
+        ObjectMapper objectMapper
+    ) {
+        Objects.requireNonNull(producer);
+        Objects.requireNonNull(objectMapper);
+        this.producer = producer;
+        this.objectMapper = objectMapper;
+    }
+
+    public void publish(EventPayload payload) {
+        String payloadJson;
+        try {
+            payloadJson = objectMapper.writeValueAsString(payload);
+        }
+        catch (JsonProcessingException ex) {
+            LOG.error(
+                "Failed to serialize event payload for sku={}",
+                payload.skuId(),
+                ex
+            );
+            return;
+        }
+
+        var message = new ProducerRecord<>(TOPIC, payload.skuId(), payloadJson);
+        var future = producer.send(message);
+
+        try {
+            // We want to wait until we know for sure Kafka published the message.
+            // TODO: We may want to add some kind of offline ROLLBACK operation to undo any database saves,
+            // or we could put them in a dead-letter-queue.
+            var metadata = future.get();
+            LOG.info(
+                "Published event: sku={}, partition={}, offset={}",
+                payload.skuId(),
+                metadata.partition(),
+                metadata.offset()
+            );
+        }
+        catch (Exception ex) {
+            LOG.error(
+                "Failed to publish event for sku={}",
+                payload.skuId(),
+                ex
+            );
+        }
+    }
+
+    public void close() {
+        producer.flush();
+        producer.close();
+    }
+}
