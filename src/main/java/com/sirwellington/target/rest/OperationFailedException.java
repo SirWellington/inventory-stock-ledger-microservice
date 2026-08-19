eba7bb5cef6688c6055fbaf8f5ac3a41a2ac7334
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-08-19 12:02:13 -0700

    Create OperationFailedException.java

---
+++ src/main/java/com/sirwellington/target/rest/OperationFailedException.java
@@ -0,0 +1,14 @@
+package com.sirwellington.target.rest;
+
+/**
+ * Thrown when a REST operation fails for an internal reason.
+ */
+public class OperationFailedException extends RuntimeException {
+    public OperationFailedException(String message) {
+        super(message);
+    }
+
+    public OperationFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
