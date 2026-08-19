af57732a6380435cbf5415381061e2d7e6b8a09f
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-08-19 12:01:26 -0700

    Better cleans up resources

--- src/main/java/com/sirwellington/target/rest/RestApplication.java
+++ src/main/java/com/sirwellington/target/rest/RestApplication.java
@@ -6,7 +6,9 @@ import com.google.inject.Guice;
 import com.sirwellington.target.db.DatabaseConfig;
 import com.sirwellington.target.db.SchemaMigration;
 import com.sirwellington.target.TargetModule;
+import com.sirwellington.target.producer.EventPublisher;
 import io.javalin.Javalin;
+import jdk.jfr.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +18,11 @@ public class RestApplication {
     private static final int PORT = Integer.parseInt(System.getProperty("port", "7070"));
 
     public static void run() {
+
         var database = DatabaseConfig.createDataSource();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+        // Ensure the database pool is cleanly closed before shutdown.
+        Runtime.getRuntime().addShutdownHook(Thread.ofVirtual().unstarted(() -> {
             LOG.info("Shutting down...");
             try {
                 database.close();
@@ -35,6 +40,12 @@ public class RestApplication {
 
         var injector = Guice.createInjector(new TargetModule(database));
 
+        // Flush and close the event publisher before shutdown.
+        var eventPublisher = injector.getInstance(EventPublisher.class);
+        Runtime.getRuntime().addShutdownHook(Thread.ofVirtual().unstarted(() -> {
+            LOG.info("Closing event publisher");
+            eventPublisher.close();
+        }));
         var healthHandler = injector.getInstance(GetHealthHandler.class);
         var recordReceiptHandler = injector.getInstance(RecordReceiptHandler.class);
         var adjustCostHandler = injector.getInstance(AdjustCostHandler.class);
